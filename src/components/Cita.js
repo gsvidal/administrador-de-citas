1c80750f5350f82b1b1bfe4443a89c242a99a7e7
Gonzo <gvidal.industrial@gmail.com>
2021-11-11 14:45:43 -0500

    Fix typo in Cita and deploy

--- src/components/Cita.js
+++ src/components/Cita.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 
 const Cita = ({cita, eliminarCita}) => (
   <div className="cita">
-    <p>Mascota:<span>{cita.mascota}</span></p>
-    <p>Mascota:<span>{cita.owner}</span></p>
-    <p>Mascota:<span>{cita.fecha}</span></p>
-    <p>Mascota:<span>{cita.hora}</span></p>
-    <p>Mascota:<span>{cita.sintomas}</span></p>
+    <p>Mascota: <span>{cita.mascota}</span></p>
+    <p>Dueño: <span>{cita.owner}</span></p>
+    <p>Fecha: <span>{cita.fecha}</span></p>
+    <p>Hora: <span>{cita.hora}</span></p>
+    <p>Sintomas: <span>{cita.sintomas}</span></p>
 
     <button
       className="button eliminar u-full-width"
